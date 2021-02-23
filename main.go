e3530f95fb3221edbf88faf6941516280b6f892d
Jorge Alfaro <jorge.alfmur@gmail.com>
2021-02-22 20:44:14 -0500

    gin web server

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("server is running ...")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hola"))
+	e := gin.Default()
+
+	e.GET("/ping", func(c *gin.Context) {
+		c.JSON(201, "pong")
 	})
 
-	if err := http.ListenAndServe(":9000", mux); err != nil {
-		log.Fatal("somenthing got wrong with your server")
+	if err := e.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
